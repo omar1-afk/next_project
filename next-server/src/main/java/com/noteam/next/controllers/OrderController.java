c2c1f6c1ef21dd33be1707d5ebcaa392fcd24bc4
hh11909 <hamo1190927@gmail.com>
2025-12-06 22:02:07 +0200

    Adding createOrder() to order controller

--- next-server/src/main/java/com/noteam/next/controllers/OrderController.java
+++ next-server/src/main/java/com/noteam/next/controllers/OrderController.java
@@ -1,9 +1,13 @@
 package com.noteam.next.controllers;
 
+import com.noteam.next.dto.OrderRequest;
 import com.noteam.next.services.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,15 @@ public class OrderController {
     @Autowired
     OrderService orderService;
     private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
+    @PostMapping
+    ResponseEntity<String> createOrder(@RequestBody OrderRequest request){
+        log.info("Order controller: Creating new order");
+        if(orderService.createOrder(request)){
+            return ResponseEntity.ok("The order is created successfully!");
+        }
+        else {
+            return ResponseEntity.badRequest().body("The weight is more than 250 KG!");
+        }
+    }
 }
