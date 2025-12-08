5e8553653023770f2afd61a64d936b7b303df432
hh11909 <hamo1190927@gmail.com>
2025-12-08 22:28:46 +0200

    the packages are untrackedso track it

--- next-server/src/main/java/com/noteam/next/controllers/OrderController.java
+++ next-server/src/main/java/com/noteam/next/controllers/OrderController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/order")
+@RequestMapping("/api/v1/order")
 public class OrderController {
     @Autowired
     OrderService orderService;
