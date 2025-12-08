bd2ff966508d43091c38e0a69c7a7b46d8f553f2
hh11909 <hamo1190927@gmail.com>
2025-12-08 22:20:21 +0200

    Adding country table and city table

---
+++ next-server/src/main/java/com/noteam/next/entities/City.java
@@ -0,0 +1,31 @@
+package com.noteam.next.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Cities")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "city_name", nullable = false)
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+    @OneToMany(mappedBy = "city",cascade = CascadeType.PERSIST)
+    private List<Order> orders;
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+}
