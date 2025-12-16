f7171ca7f4f47771ba5cdb4266e5aac29d360ac7
nesmaelsayed393 <nesmaashour263@gmail.com>
2025-12-16 21:09:39 +0200

    update

--- next-client/src/main/java/module-info.java
+++ next-client/src/main/java/module-info.java
@@ -1,6 +1,11 @@
 module org.noteam.nextclient {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jdk.jsobject;
+    requires java.desktop;
+    requires android.json;
+    requires com.fasterxml.jackson.core;
+    requires com.google.gson;
 
 
     opens org.noteam.nextclient to javafx.fxml;
