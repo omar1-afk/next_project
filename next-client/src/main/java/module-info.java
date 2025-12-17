70e6c13aa4e2c6d786a3078cc8d78916d6dfa53d
Amr Khaled <redwolf4325@gmail.com>
2025-12-17 11:19:40 +0200

    Fixed some issues in the MainController

--- next-client/src/main/java/module-info.java
+++ next-client/src/main/java/module-info.java
@@ -1,22 +1,28 @@
 module org.noteam.nextclient {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires io.github.cdimascio.dotenv.java;
-    requires java.net.http;
-    requires com.fasterxml.jackson.databind;
-    requires spring.webflux;
-    requires spring.web;
-    requires reactor.core;
-    requires org.reactivestreams;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires io.github.cdimascio.dotenv.java;
+  requires java.net.http;
+  requires com.fasterxml.jackson.databind;
+  requires spring.webflux;
+  requires spring.web;
+  requires reactor.core;
+  requires org.reactivestreams;
 
+  opens org.noteam.nextclient to javafx.fxml;
 
-    opens org.noteam.nextclient to javafx.fxml;
-    exports org.noteam.nextclient.scene to javafx.graphics;
-    opens org.noteam.nextclient.controller to javafx.fxml;
-    opens org.noteam.nextclient.entity to com.fasterxml.jackson.databind;
-    exports org.noteam.nextclient;
-    opens org.noteam.nextclient.controller to javafx.fxml;
-    exports org.noteam.nextclient.controller;
-    opens org.noteam.nextclient.scene to javafx.fxml;
-    exports org.noteam.nextclient.scene;
+  exports org.noteam.nextclient.scene to javafx.graphics;
+
+  opens org.noteam.nextclient.entity to com.fasterxml.jackson.databind;
+
+  exports org.noteam.nextclient;
+
+  opens org.noteam.nextclient.controller to javafx.fxml;
+
+  exports org.noteam.nextclient.controller;
+
+  opens org.noteam.nextclient.scene to javafx.fxml;
+
+  opens org.noteam.nextclient.assets to javafx.fxml;
+
 }
--- next-client/src/main/java/org/noteam/nextclient/scene/MainScene.java
+++ next-client/src/main/java/org/noteam/nextclient/scene/MainScene.java
@@ -11,20 +11,21 @@ import javafx.stage.Stage;
 import java.util.Objects;
 
 public class MainScene extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
-        Scene scene=new Scene(root,1440,720);
-        stage.setTitle("Next Application");
-        String css = getClass().getResource("style.css").toExternalForm();
-        // In Main.java
-        Font.loadFont(getClass().getResourceAsStream("/org/noteam/nextclient/fonts/Roboto-Regular.ttf"), 12);
-        scene.getStylesheets().add(css);
-        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/noteam/nextclient/assets/logo.png")));
-        stage.getIcons().add(icon);
-        stage.setResizable(false);
-        stage.setFullScreen(true);
-        stage.setScene(scene);
-        stage.show();
-    }
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+    Scene scene = new Scene(root, 1440, 720);
+    stage.setTitle("Next Application");
+    String css = getClass().getResource("style.css").toExternalForm();
+    // In Main.java
+    Font.loadFont(getClass().getResourceAsStream("/org/noteam/nextclient/fonts/Roboto-Regular.ttf"), 12);
+    scene.getStylesheets().add(css);
+    Image icon = new Image(
+        Objects.requireNonNull(getClass().getResourceAsStream("/org/noteam/nextclient/assets/logo.png")));
+    stage.getIcons().add(icon);
+    stage.setResizable(true);
+    stage.setFullScreen(false);
+    stage.setScene(scene);
+    stage.show();
+  }
 }
