a6f54ce3be4da5a8f169783688c3a4d173a35942
Amr Khaled <redwolf4325@gmail.com>
2025-12-17 20:01:07 +0200

    Finished the pop up window to show shipment details But there are somethings i did not handle yet - Dropdown filters

--- next-client/src/main/java/org/noteam/nextclient/controller/ShipmentController.java
+++ next-client/src/main/java/org/noteam/nextclient/controller/ShipmentController.java
@@ -1,13 +1,70 @@
 package org.noteam.nextclient.controller;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.noteam.nextclient.dto.Order;
+import org.noteam.nextclient.dto.ShipmentDetails;
+import org.noteam.nextclient.dto.State;
+import org.noteam.nextclient.scene.DataEntryWindow;
+
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.stage.Stage;
 
 public class ShipmentController {
-    @FXML
-    private BorderPane shipmentPane;
-    public BorderPane getShipmentPane(){
-        return shipmentPane;
-    }
+  @FXML
+  private BorderPane shipmentPane;
+
+  public BorderPane getShipmentPane() {
+    return shipmentPane;
+  }
+
+  @FXML
+  private Button openPopUp;
+
+  @FXML
+  protected void showPopUp(ActionEvent e) throws IOException {
+    DataEntryWindow window = new DataEntryWindow<>("Total", (Stage) ((Node) e.getSource()).getScene().getWindow(),
+        1280,
+        720);
+    LocalDate d = LocalDate.of(2025, 12, 31);
+    Order order = new Order(
+        0, "EG", "Alex", "Borg", "District", false, true, 100, State.PICKED, 50, d, 1,
+        LocalDateTime.now(),
+        LocalDate.now() // Use LocalDate.now() here to match the record
+    );
+    ArrayList<Order> orders = new ArrayList<>();
+    orders.add(order);
+    ShipmentDetails shipment = new ShipmentDetails(
+        0,
+        orders);
+    FXMLLoader loader = new FXMLLoader(
+        getClass().getResource("/org/noteam/nextclient/scene/shipmentDetails-scene.fxml"));
+
+    Parent layout = loader.load();
+
+    window.setContent((Region) layout);
+
+    ShipmentDetailsController controller = loader.getController();
+    controller.setShipmentDetails(shipment);
+
+    Optional<Object> s = window.showAndWaitForResult();
+  }
 
 }
--- next-client/src/main/java/org/noteam/nextclient/controller/ShipmentDetailsController.java
+++ next-client/src/main/java/org/noteam/nextclient/controller/ShipmentDetailsController.java
@@ -1,4 +1,102 @@
 package org.noteam.nextclient.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.noteam.nextclient.dto.ShipmentDetails;
+import org.noteam.nextclient.dto.Order;
+import org.noteam.nextclient.dto.State;
+
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ShipmentDetailsController {
+
+  private ShipmentDetails shipmentDetails = new ShipmentDetails(
+      0, new ArrayList<>());
+
+  public void setShipmentDetails(ShipmentDetails shipmentDetails) {
+
+    if (idColumn.getCellValueFactory() == null) {
+
+      idColumn.setCellValueFactory(data -> data.getValue().id());
+      weightColumn.setCellValueFactory(data -> data.getValue().weight().asObject());
+      statusColumn.setCellValueFactory(data -> data.getValue().status());
+      priceColumn.setCellValueFactory(data -> data.getValue().price().asObject());
+    }
+
+    this.shipmentDetails = shipmentDetails;
+    refresh();
+  }
+
+  @FXML
+  private TableColumn<OrderDetails, String> idColumn;
+  @FXML
+  private TableColumn<OrderDetails, Integer> weightColumn;
+  @FXML
+  private TableColumn<OrderDetails, String> statusColumn;
+  @FXML
+  private TableColumn<OrderDetails, Float> priceColumn;
+
+  private void refresh() {
+    Integer shipmentId = shipmentDetails.id();
+    shipmentIdLabel.setText(shipmentId.toString());
+    ObservableList<OrderDetails> orders = prepareOrders();
+    ordersTable.setItems(orders);
+  }
+
+  public record OrderDetails(
+      SimpleStringProperty id,
+      SimpleIntegerProperty weight,
+      SimpleStringProperty status,
+      SimpleFloatProperty price) {
+  }
+
+  private String statusToString(State state) {
+    switch (state) {
+      case PICKED:
+        return "Picked";
+      case PACKAGING:
+        return "In packaging process";
+      case SHIPPING:
+        return "Shipping";
+      case DELEVERED:
+        return "Delivered";
+      case RETURNED:
+        return "Returned";
+      default:
+        return "NONE";
+    }
+  }
+
+  private ObservableList<OrderDetails> prepareOrders() {
+    ArrayList<OrderDetails> orderDetailsList = new ArrayList<>();
+    for (Order order : shipmentDetails.orders()) {
+      OrderDetails orderDetails = new OrderDetails(
+          new SimpleStringProperty(
+              "Order Id: " + order.id()),
+          new SimpleIntegerProperty(order.weight()),
+          new SimpleStringProperty(
+              statusToString(order.state())),
+          new SimpleFloatProperty(order.price()));
+      orderDetailsList.add(orderDetails);
+    }
+    ;
+    return FXCollections.observableArrayList(orderDetailsList);
+  };
+
+  @FXML
+  private Label shipmentIdLabel;
+  @FXML
+  private TableView ordersTable;
+
 }
